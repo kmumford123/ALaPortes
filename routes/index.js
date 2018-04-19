8fdb6033f51af5dc5fb096d9db0df66e9b17a99d
Khamel <khamel.mumford@gmail.com>
2018-04-19 18:23:38 -0500

    server contstructs created.  React not loading on node start

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const newsRouter = require("./apiRoutes");
+const htmlRouter = require("./htmlRoutes")
+    // console.log(newsRouter);
+
+
+router.use("/", newsRouter)
+router.use("/", htmlRouter)
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const routes = require("./routes")
+const Router = express.Router()
+var bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("at-the-gate/build"));
+}
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./at-the-gate/build/index.html"));
+});
+
+// // Our scraping tools
+// // Axios is a promised-based http library, similar to jQuery's Ajax method
+// // It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+mongoose.connect(MONGODB_URI);
+
+// Routes
+// app.use(routes);
+
+
+app.listen(PORT, function() {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
